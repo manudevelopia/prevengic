22dcead1ab8510e2608b002b8f222fff34860147
Manu Garcia <info@developia.info>
2018-12-01 10:09:56 +0100

    Init repository

---
+++ src/main/java/info/developia/prevengic/GikApplication.java
@@ -0,0 +1,12 @@
+package info.developia.prevengic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GikApplication.class, args);
+    }
+}

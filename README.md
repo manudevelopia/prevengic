0b370ae50bec33281e2a8a0409e5ba941221cf66
Manu Garcia <info@developia.info>
2018-12-02 03:17:59 +0100

    Add front libs

---
+++ README.md
@@ -0,0 +1,19 @@
+#PrevenGIC
+
+## Description
+This application tries to support Chemical pollution contaminators calculation on Safety & Security report
+
+## Application
+This application has been developed with:
+ - SpringBoot
+ - React
+ - Postgres 
+
+## Deploy
+You can have a try to this app at https://prevengic.herokuapp.com/
+
+
+## To do
+- Compilar el front y generar el bundle.js forma automatizada en el despliegue
+
+
--- src/main/js/src/components/Hello.tsx
+++ src/main/js/src/components/Hello.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import {Alert} from "reactstrap";
 
 export interface HelloProps {
     compiler: string;
@@ -10,7 +11,13 @@ export interface HelloProps {
 export class Hello extends React.Component<HelloProps, {}> {
 
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <div>
+            <Alert color="primary">
+                This is a primary alert — check it out!
+            </Alert>
+
+            <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+        </div>
     }
-    
+
 }

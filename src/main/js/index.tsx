62014804f5a2d7271ac1cb33329feb2a4c8d44e1
Manu Garcia <info@developia.info>
2018-12-15 10:07:22 +0100

    Draw some Bootstrap elements as a concept

--- src/main/js/index.tsx
+++ src/main/js/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import {Hello} from "./components/Hello";
+import {Layout} from "./components/Layout";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React"/>,
-    document.getElementById("example")
+    <Layout compiler="TypeScript" framework="React"/>,
+    document.getElementById("layout")
 );

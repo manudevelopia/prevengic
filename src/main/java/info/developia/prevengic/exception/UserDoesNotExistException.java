314f39faceb64b9df12c1bc4f8689f5efc90edb9
manudevelopia <manu@developia.info>
2019-04-04 19:17:19 +0200

    Add exception centralized manager

--- src/main/java/info/developia/prevengic/exception/UserDoesNotExistException.java
+++ src/main/java/info/developia/prevengic/exception/UserDoesNotExistException.java
@@ -1,9 +1,13 @@
 package info.developia.prevengic.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User not found")
 public class UserDoesNotExistException extends RuntimeException {
+
+    public UserDoesNotExistException() {
+        super("User does not exist, please change search criteria");
+    }
+
+    public UserDoesNotExistException(String message) {
+        super(message);
+    }
 
 }

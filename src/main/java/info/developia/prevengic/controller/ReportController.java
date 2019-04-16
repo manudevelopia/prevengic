893b82eb557da6a504391f6ca4188a09150a3a62
manudevelopia <manu@developia.info>
2019-04-16 07:04:53 +0200

    feat: implement report controller with test

---
+++ src/main/java/info/developia/prevengic/controller/ReportController.java
@@ -0,0 +1,39 @@
+package info.developia.prevengic.controller;
+
+import info.developia.prevengic.dao.Report;
+import info.developia.prevengic.dto.SelectedCompoundForm;
+import info.developia.prevengic.service.ReportService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reports")
+public class ReportController {
+
+    private final ReportService reportService;
+
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @GetMapping("/all")
+    ResponseEntity<List<Report>> getAll() {
+        List<Report> reports = reportService.findAll();
+
+        return ResponseEntity.ok(reports);
+    }
+
+    @PostMapping("/create")
+    ResponseEntity create(@RequestBody SelectedCompoundForm selectedCompoundForm) {
+        Report report = reportService.create(selectedCompoundForm);
+
+        return ResponseEntity.ok(report);
+    }
+
+}
---
+++ src/test/groovy/info/developia/prevengic/controller/ReportControllerTest.groovy
@@ -0,0 +1,62 @@
+package info.developia.prevengic.controller
+
+import info.developia.prevengic.dao.Report
+import info.developia.prevengic.dto.SelectedCompoundForm
+import info.developia.prevengic.dto.SelectedCompoundItem
+import info.developia.prevengic.service.ReportService
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import spock.lang.Specification
+
+import static org.mockito.ArgumentMatchers.any
+import static org.mockito.Mockito.when
+
+class ReportControllerTest extends Specification {
+
+    @Mock
+    ReportService reportService
+
+    @InjectMocks
+    ReportController reportController
+
+    SelectedCompoundForm compoundReportForm
+
+    def setup() {
+        MockitoAnnotations.initMocks(this)
+
+        compoundReportForm = new SelectedCompoundForm(compounds: [
+                new SelectedCompoundItem(id: 1L, concentration: 1D, exposition: 2D),
+                new SelectedCompoundItem(id: 2L, concentration: 2D, exposition: 3D),
+                new SelectedCompoundItem(id: 3L, concentration: 3D, exposition: 4D),
+                new SelectedCompoundItem(id: 4L, concentration: 4D, exposition: 5D)
+        ])
+    }
+
+    def "test get All"() {
+        given:
+        when(reportService.findAll()).thenReturn([new Report()])
+
+        when:
+        ResponseEntity<List<Report>> result = reportController.getAll()
+
+        then:
+        result.getStatusCode() == HttpStatus.OK
+        result.getBody() != null
+    }
+
+    def "test create"() {
+        given:
+        when(reportService.create(any() as SelectedCompoundForm)).thenReturn(new Report())
+
+        when:
+        ResponseEntity result = reportController.create(compoundReportForm)
+
+        then:
+        result.getStatusCode() == HttpStatus.OK
+        result.getBody() != null
+    }
+
+}

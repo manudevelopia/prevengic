1eeb6e84d73ae5af63617de30269099f51a0a1f8
manudevelopia <manu@developia.info>
2019-04-16 18:56:24 +0200

    feat: add ReportService to create reports from form

--- src/main/java/info/developia/prevengic/service/ReportService.java
+++ src/main/java/info/developia/prevengic/service/ReportService.java
@@ -1,11 +1,14 @@
 package info.developia.prevengic.service;
 
 import info.developia.prevengic.dao.Report;
+import info.developia.prevengic.dto.SelectedCompoundForm;
 
 import java.util.List;
 
 public interface ReportService {
 
     List<Report> findAll();
+
+    Report create(SelectedCompoundForm selectedCompoundForm);
 
 }
--- src/main/java/info/developia/prevengic/service/ReportServiceImpl.java
+++ src/main/java/info/developia/prevengic/service/ReportServiceImpl.java
@@ -1,23 +1,58 @@
 package info.developia.prevengic.service;
 
 import info.developia.prevengic.dao.Report;
+import info.developia.prevengic.dto.SelectedCompoundForm;
+import info.developia.prevengic.dto.SelectedCompoundItem;
+import info.developia.prevengic.model.Compound;
+import info.developia.prevengic.model.CompoundReportResult;
 import info.developia.prevengic.repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-public class ReportServiceImpl implements ReportService{
+public class ReportServiceImpl implements ReportService {
 
     private final ReportRepository reportRepository;
 
-    public ReportServiceImpl(ReportRepository reportRepository) {
+    private final CompoundService compoundService;
+
+    @Autowired
+    public ReportServiceImpl(ReportRepository reportRepository, CompoundService compoundService) {
         this.reportRepository = reportRepository;
+        this.compoundService = compoundService;
     }
 
     @Override
     public List<Report> findAll() {
         return reportRepository.findAll();
+    }
+
+    @Override
+    public Report create(SelectedCompoundForm selectedCompoundForm) {
+        Report report = new Report();
+
+        // TODO: add compounds results to entity
+        List<CompoundReportResult> result = processSelectedCompounds(selectedCompoundForm);
+
+        return reportRepository.save(report);
+    }
+
+    private List<CompoundReportResult> processSelectedCompounds(SelectedCompoundForm selectedCompoundForm) {
+        return selectedCompoundForm.getCompounds().stream()
+                .map(this::processCompound)
+                .collect(Collectors.toList());
+    }
+
+    private CompoundReportResult processCompound(SelectedCompoundItem selectedCompoundItem) {
+        Compound compound = compoundService.findById(selectedCompoundItem.getId());
+
+        // TODO: calculate with compound with compound exposure and concentration
+
+        return CompoundReportResult.builder()
+                .build();
     }
 
 }

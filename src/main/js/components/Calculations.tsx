121d37a025be6fb7e0e7beb69de83dca974be678
manudevelopia <manugarciarodriguez@gmail.com>
2019-04-20 10:58:10 +0200

    chore: change table title literal

--- src/main/js/components/Calculations.tsx
+++ src/main/js/components/Calculations.tsx
@@ -27,7 +27,7 @@ export class Calculations extends React.Component<any, any> {
     return (
       <Row>
         <Col md={12}>
-          <h4>Resultados</h4>
+          <h4>Resultado del cálculo</h4>
           <Table>
             <thead>
             <tr>

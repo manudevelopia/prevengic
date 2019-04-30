a7960afbbe035f6a79fb4e2422bea48158ddbeea
manudevelopia <manu@developia.info>
2019-04-30 18:55:16 +0200

    feat: add react-redux inital action and reducer

--- src/main/js/index.tsx
+++ src/main/js/index.tsx
@@ -5,27 +5,10 @@ import {createStore} from 'redux';
 
 import {Layout} from "./components/Layout";
 
-const initialState = {
-  count: 0
-};
+import rootReducer from './reducers';
 
-function reducer(state = initialState, action: any) {
-  console.log('reducer', state, action);
 
-  switch (action.type) {
-    case 'INCREMENT':
-      return {count: state.count + 1};
-    case 'DECREMENT':
-      return {copunt: state.count - 1};
-    case 'RESET':
-      return {count: 0};
-    default:
-      return state;
-  }
-
-}
-
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 store.dispatch({type: "INCREMENT"});
 store.dispatch({type: "DECREMENT"});

88836ed6b6696b88314f541cc14eaeba058c8b55
manudevelopia <manu@developia.info>
2019-04-30 13:31:01 +0200

    feat: add react-redux inital concept

--- src/main/js/index.tsx
+++ src/main/js/index.tsx
@@ -1,9 +1,39 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 import {Layout} from "./components/Layout";
 
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action: any) {
+  console.log('reducer', state, action);
+
+  switch (action.type) {
+    case 'INCREMENT':
+      return {count: state.count + 1};
+    case 'DECREMENT':
+      return {copunt: state.count - 1};
+    case 'RESET':
+      return {count: 0};
+    default:
+      return state;
+  }
+
+}
+
+const store = createStore(reducer);
+
+store.dispatch({type: "INCREMENT"});
+store.dispatch({type: "DECREMENT"});
+store.dispatch({type: "RESET"});
+
 ReactDOM.render(
-    <Layout compiler="TypeScript" framework="React"/>,
+  <Provider store={store}>
+    <Layout compiler="TypeScript" framework="React"/>
+  </Provider>,
     document.getElementById("layout")
 );

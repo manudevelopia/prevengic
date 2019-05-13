9803d56644cb73ecc0e91be8b9a9f2c061b50f2a
manudevelopia <manu@developia.info>
2019-05-13 14:25:38 +0200

    test: m igrate test from Mockito to Spock

--- src/test/groovy/info/developia/prevengic/controller/CompoundControllerTest.groovy
+++ src/test/groovy/info/developia/prevengic/controller/CompoundControllerTest.groovy
@@ -2,22 +2,13 @@ package info.developia.prevengic.controller
 
 import info.developia.prevengic.model.Compound
 import info.developia.prevengic.service.CompoundService
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
 import org.springframework.http.ResponseEntity
 import spock.lang.Specification
 
-import static org.mockito.Mockito.when
-
 class CompoundControllerTest extends Specification {
 
-    @Mock
     CompoundService compoundService
-
-    @InjectMocks
     CompoundController compoundController
-
     Compound compound
 
     def name = "Test compound"
@@ -25,69 +16,70 @@ class CompoundControllerTest extends Specification {
     def nce = "nce"
 
     def setup() {
-        MockitoAnnotations.initMocks(this)
+        compoundService = Mock()
+        compoundController = new CompoundController(compoundService)
 
         compound = new Compound(cas: cas, nce: nce, name: name)
     }
 
     def "test get All"() {
         given:
-        when(compoundService.findAll()).thenReturn([compound])
+        compoundService.findAll() >> [compound]
 
         when:
         ResponseEntity<List<Compound>> results = compoundController.getAll()
 
         then:
-        def result = results.getBody().get(0)
-
-        result.name == name
-        result.cas == cas
-        result.nce == nce
+        verifyAll(results.getBody().get(0)) {
+            name == this.name
+            cas == this.cas
+            nce == this.nce
+        }
     }
 
     def "test find By Name"() {
         given:
-        when(compoundService.findBy(null, null, name)).thenReturn([compound])
+        compoundService.findBy(null, null, name) >> [compound]
 
         when:
         ResponseEntity<List<Compound>> results = compoundController.findBy(null, null, name)
 
         then:
-        def result = results.getBody().get(0)
-
-        result.name == name
-        result.cas == cas
-        result.nce == nce
+        verifyAll(results.getBody().get(0)) {
+            name == this.name
+            cas == this.cas
+            nce == this.nce
+        }
     }
 
     def "test find By Nce"() {
         given:
-        when(compoundService.findBy("nce", null, null)).thenReturn([compound])
+        compoundService.findBy(nce, null, null) >> [compound]
 
         when:
         ResponseEntity<List<Compound>> results = compoundController.findBy(nce, null, null)
 
         then:
-        def result = results.getBody().get(0)
-
-        result.name == name
-        result.cas == cas
-        result.nce == nce
+        verifyAll(results.getBody().get(0)) {
+            name == this.name
+            cas == this.cas
+            nce == this.nce
+        }
     }
 
     def "test find By Cas"() {
         given:
-        when(compoundService.findBy(null, "cas", null)).thenReturn([compound])
+        compoundService.findBy(null, cas, null) >> [compound]
 
         when:
         ResponseEntity<List<Compound>> results = compoundController.findBy(null, cas, null)
 
         then:
-        def result = results.getBody().get(0)
-
-        result.name == name
-        result.cas == cas
-        result.nce == nce
+        verifyAll(results.getBody().get(0)) {
+            name == this.name
+            cas == this.cas
+            nce == this.nce
+        }
     }
 
 }

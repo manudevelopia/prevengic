57efb6faa31f2f079ee7aac482b99014b929d078
manudevelopia <manu@developia.info>
2019-07-22 14:35:49 +0200

    fix: make selected compunds list match http petition

--- src/main/js/components/CompoundsForm.tsx
+++ src/main/js/components/CompoundsForm.tsx
@@ -67,6 +67,11 @@ export class CompoundsForm extends React.Component<any, any> {
   }
 
   private removeFromSelected(index: number) {
+    let results = this.state.compounds.slice();
+
+    results.splice(this.state.compounds[index], 1);
+    this.setState({compounds: results});
+
     this.props.onChange(index);
   }
 

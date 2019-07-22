1e89e22f35109734b10fa58aa2a066abd00ad447
manudevelopia <manu@developia.info>
2019-07-22 11:45:27 +0200

    refactor: use bootstrap as dependency instead of static file

--- src/main/js/index.tsx
+++ src/main/js/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {Layout} from "./components/Layout";
 
 ReactDOM.render(
